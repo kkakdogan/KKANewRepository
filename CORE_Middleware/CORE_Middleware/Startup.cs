0da4f0b0637b6eb5b24881a02bcb16380ec4aa86
Kartal Kaan Akdoğan <kartalkaanakdogan@hotmail.com>
2020-11-24 10:32:36 +0300

    YMS 5474 Deneme

---
+++ CORE_Middleware/CORE_Middleware/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CORE_Middleware
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+            {
+
+                if (context.Request.Path == "/profile")
+                {
+                    await context.Response.WriteAsync("Kullanıcının bilgileri");
+                }
+                else
+                {
+                    await next();
+                }
+
+
+            });
+
+            app.Map("/orders",
+                config =>
+                config.Use(async (context, next) =>
+                await context.Response.WriteAsync("Kullanicinin siparişleri")
+                ));
+
+            app.MapWhen(
+                context => context.Request.Method == "POST" &&
+                           context.Request.Path == "/account",
+                config =>
+                            config.Use(async (context, next) =>
+                                            await context.Response.WriteAsync("Accounta Post işlemi yapıldığında çalışacaktır.")
+
+            ));
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
